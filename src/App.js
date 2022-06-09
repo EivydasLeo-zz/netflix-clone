9842a2e0d4c258ade9346d951bc7c599c42b8631
Eivydas <eivydas.leonavicius@nfq.lt>
2022-06-09 10:02:08 +0300

    Nav component implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles/main.scss";
 import MovieList from "./components/MovieList";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 
 const App = () => {
 
   return (
     <div className="wrapper">
+        <Nav/>
         <Banner />
        <MovieList />
     </div>
